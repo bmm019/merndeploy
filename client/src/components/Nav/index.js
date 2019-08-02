f0bf6f3d8eefa8c3d92e82b92db5ed6a58b177b1
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-08-01 23:27:49 -0500

    fixed saved book route

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Google Book Search
       </a>
+      <div id="navbarNav">
+          <ul className="navbar-nav">
+              <li className="nav-item" id="home">
+                  <a className="nav-link" href="/"><button type="button" className="btn">Search Books</button></a>
+              </li>
+              <li className="nav-item" id="report">
+                  <a className="nav-link" href="/saved"><button type="button" className="btn">Saved Books</button></a>
+              </li>
+          </ul>
+      </div>
     </nav>
   );
 }
